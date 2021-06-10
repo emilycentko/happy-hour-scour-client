83b08b1e31c7b2ad1be8eb6cabd8ba9ef8ae631a
emilycentko <emily.centko@gmail.com>
2021-06-10 15:03:29 -0500

    Added basic nav bar and logout functionality

---
+++ src/components/nav/NavBar.js
@@ -0,0 +1,52 @@
+import React from "react"
+import { Link, useHistory } from "react-router-dom"
+import "./NavBar.css"
+import Logo from "./HHSlogo.png"
+
+export const NavBar = () => {
+    const history = useHistory()
+
+    return (
+        <>
+            <div className="header">
+                <ul className="navbar">
+                    <li className="logo">
+                        <img className="navbar__logo" src={Logo} />
+                    </li>
+                    <li className="navbar__item">
+                        <Link className="navbar__link" to="/">Home</Link>
+                    </li>
+                    <li className="navbar__item">
+                        <Link className="navbar__link" to="/favorites">Favorites</Link>
+                    </li>
+                    <li className="navbar__item">
+                        <Link className="navbar__link" to="/profile">Profile</Link>
+                    </li>
+                   
+                    <div className="navbar__item">
+                        {
+                            (localStorage.getItem("hhs_token") !== null) ?
+                                <li className="navbar__item">
+                                    <button className="nav-link fakeLink navbar__item"
+                                        onClick={() => {
+                                            localStorage.removeItem("hhs_token")
+                                            history.push({ pathname: "/" })
+                                        }}
+                                    >Logout</button>
+                                </li> :
+                                <>
+                                    <li className="nav-item">
+                                        <Link className="nav-link" to="/login">Login</Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link className="nav-link" to="/register">Register</Link>
+                                    </li>
+                                </>
+                        }
+                    </div>
+                </ul>
+                <hr></hr>
+            </div>
+        </>
+    )
+}
