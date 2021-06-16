123398f2a94a7417c0a645dbb9c5e3467552062f
emilycentko <emily.centko@gmail.com>
2021-06-16 15:33:16 -0500

    Moved happy hour to individual card component and calling HappyHourCard in List component .map in order to set individual state for add to favorites button

--- src/components/happyhour/HappyHourList.js
+++ src/components/happyhour/HappyHourList.js
@@ -1,12 +1,12 @@
 import React, { useContext, useEffect, useState } from "react"
 import { HappyHourContext } from "./HappyHourProvider.js"
 import { useHistory, useLocation } from 'react-router-dom'
-import Heart from "react-heart"
-import Card from 'react-bootstrap/Card'
-import Button from 'react-bootstrap/Button'
 import CardDeck from 'react-bootstrap/CardDeck'
 import { FavoriteContext } from "../favorites/FavoriteProvider.js"
+import { HappyHourCard } from "./HappyHourCard.js"
 import "./HappyHour.css"
+import Row from 'react-bootstrap/Row'
+import Container from 'react-bootstrap/Container'
 
 export const HappyHourList = () => {
 
@@ -32,32 +32,19 @@ export const HappyHourList = () => {
         <>
             <h1 className="happy__hours_title">{weekday ? `${weekday}'s Happy Hours` : "Today's Happy Hours"}</h1>
             <div className="happy_hours">
-                <CardDeck>
+            <Container>
+                <Row className="justify-content-md-left">
+           
                 {
                     happyhours.map(happyhour => {
-                        console.log(happyhour.id)
-                        return <section key={`happyhour--${happyhour.id}`} className="happyhour">
                         
-                        <Card style={{ width: '18rem' }}>
-                            <Card.Img src={happyhour.image} />
-                            <Card.Body>
-                                <Card.Title className="happyhour__business">{happyhour.business.name}</Card.Title>
-                                <Card.Text>
-                                    
-                                    <div className="happyhour__description">{happyhour.description}</div>
-                                </Card.Text>
-                           </Card.Body>
-                            <Card.Footer>
-                                <Button variant="primary">Reviews</Button>
-                           <button className="happyhour__heart" 
-			                    onClick={() => addFavorite(happyhour.id)}>
-		                    </button>
-                            </Card.Footer>
-                        </Card>
-                    </section>
+                        return <HappyHourCard key={happyhour.id} happyhour={happyhour} />
+
                     })
                 }
-                </CardDeck>
+            
+                </Row>
+            </Container>
             </div>
         </>
     )
