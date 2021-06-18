b1c03986a6564b9320592ca5b6525ce2718c183a
emilycentko <emily.centko@gmail.com>
2021-06-18 12:46:40 -0500

    Refactored favorites list to include own favorite card component to be able to remove a favorite from /favorites tab, and it is removed from DOM without refreshing page

---
+++ src/components/favorites/FavoriteCard.js
@@ -0,0 +1,56 @@
+
+import React, { useContext, useState, useEffect } from "react"
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import { FavoriteContext } from "../favorites/FavoriteProvider.js"
+import Heart from "react-heart"
+import { HappyHourContext } from "../happyhour/HappyHourProvider.js"
+import { useLocation } from 'react-router-dom'
+
+export const FavoriteCard = ({favorite}) => {
+
+    const {addFavorite, removeFavorite} = useContext(HappyHourContext)
+    const {favorites, getFavorites} = useContext(FavoriteContext)
+    // const [active, setActive] = useState(false)
+
+    const day = useLocation()
+    const [_, weekday] = day.search.split("=")
+
+
+    return (
+        
+        <Card style={{ width: '18rem' }} className="happyhour">
+            <Card.Img src={favorite.happy_hour.image} />
+            <Card.Body>
+                <Card.Title className="happyhour__business">{favorite.happy_hour.business.name}</Card.Title>
+                    <Card.Text>
+                                    
+                        <div className="happyhour__description">{favorite.happy_hour.description}</div>
+                    </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+                <Button variant="primary">Reviews</Button>
+                    
+                     {favorite.happy_hour 
+                    
+                    ? <button className="happyhour__heart" 
+                        onClick={() =>
+                            removeFavorite(favorite.happy_hour.id)
+                            .then(() => getFavorites())}
+                            >
+                            Remove
+                        </button>
+                    : <button className="happyhour__heart" 
+                        onClick={() => 
+                            addFavorite(favorite.happy_hour.id)
+                            .then(() => getFavorites())}
+                            >
+                            Add
+                    </button>
+                    }
+                    
+            </Card.Footer>
+        </Card>
+        
+    )
+}
--- src/components/favorites/FavoriteList.js
+++ src/components/favorites/FavoriteList.js
@@ -5,6 +5,9 @@ import Heart from "react-heart"
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
 import CardDeck from 'react-bootstrap/CardDeck'
+import Row from 'react-bootstrap/Row'
+import Container from 'react-bootstrap/Container'
+import { FavoriteCard } from "./FavoriteCard"
 
 export const FavoriteList = () => {
 
@@ -22,34 +25,56 @@ export const FavoriteList = () => {
 
     return (
         <>
-            {/* <h1 className="happy__hours_title">{weekday ? `Your Favorite Happy Hours on ${weekday}` : "Your Favorite Happy Hours Today"}</h1> */}
-            <h1>Favorites</h1>
-            <div className="all__happy_hours">
-                <CardDeck>
-                {
+            <h1 className="happy__hours_title">Favorites</h1>
+            <div className="happy_hours">
+                <Container>
+                    <Row className="justify-content-md-left">
+           
+                    {
                     favorites.map(favorite => {
-                        return <section key={`happyhour--${favorite.happy_hour.id}`} className="happyhour">
                         
-                        <Card style={{ width: '18rem' }}>
-                            <Card.Img src={favorite.happy_hour.image} />
-                            <Card.Body>
-                                <Card.Title className="happyhour__business">{favorite.happy_hour.business.name}</Card.Title>
-                                <Card.Text>
-                                    <div className="happyhour__description">{favorite.happy_hour.description}</div>
-                                </Card.Text>
-                           </Card.Body>
-                            <Card.Footer>
-                                <Button variant="primary">Reviews</Button>
-                           {/* <button className="happyhour__heart" style={{ width: "2rem" }}>
-			                    <Heart isActive={active} onClick={() => setActive(!active)}/>
-		                    </button> */}
-                            </Card.Footer>
-                        </Card>
-                    </section>
-                    })
-                }
-                </CardDeck>
+                        return <FavoriteCard key={favorite.id} favorite={favorite} />
+
+                        })
+                    }
+            
+                    </Row>
+                </Container>
             </div>
         </>
     )
 }
+
+//     return (
+//         <>
+//             {/* <h1 className="happy__hours_title">{weekday ? `Your Favorite Happy Hours on ${weekday}` : "Your Favorite Happy Hours Today"}</h1> */}
+//             <h1>Favorites</h1>
+//             <div className="all__happy_hours">
+//                 <CardDeck>
+//                 {
+//                     favorites.map(favorite => {
+//                         return <section key={`happyhour--${favorite.happy_hour.id}`} className="happyhour">
+                        
+//                         <Card style={{ width: '18rem' }}>
+//                             <Card.Img src={favorite.happy_hour.image} />
+//                             <Card.Body>
+//                                 <Card.Title className="happyhour__business">{favorite.happy_hour.business.name}</Card.Title>
+//                                 <Card.Text>
+//                                     <div className="happyhour__description">{favorite.happy_hour.description}</div>
+//                                 </Card.Text>
+//                            </Card.Body>
+//                             <Card.Footer>
+//                                 <Button variant="primary">Reviews</Button>
+//                            {/* <button className="happyhour__heart" style={{ width: "2rem" }}>
+// 			                    <Heart isActive={active} onClick={() => setActive(!active)}/>
+// 		                    </button> */}
+//                             </Card.Footer>
+//                         </Card>
+//                     </section>
+//                     })
+//                 }
+//                 </CardDeck>
+//             </div>
+//         </>
+//     )
+// }
